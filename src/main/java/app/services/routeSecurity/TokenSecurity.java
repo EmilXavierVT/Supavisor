4fd8398986c4973a4fb168cb3e7f034857c5c76b
EmilXavierVT <emilxavierthorsen@gmail.com>
2026-09-17 12:15:05 +0200

    added tenantID to the claimset

--- src/main/java/app/services/routeSecurity/TokenSecurity.java
+++ src/main/java/app/services/routeSecurity/TokenSecurity.java
@@ -59,6 +59,7 @@ public class TokenSecurity implements ITokenSecurity {
                     .issuer(ISSUER)
                     .claim("email", user.getEmail())
                     .claim("roles", user.getRoles().stream().reduce((s1, s2) -> s1 + "," + s2).get())
+                    .claim("tenantId",user.getTenantId())
                     .expirationTime(new Date(new Date().getTime() + Long.parseLong(TOKEN_EXPIRE_TIME)))
                     .build();
             SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), claimsSet);
