544142f593f3de45980e490351781e2c9e6ca74c
frede <frededv@outlook.dk>
2026-09-21 17:11:28 +0200

    bugfix, users active status was reversed

--- src/main/java/app/dto/UserDTO.java
+++ src/main/java/app/dto/UserDTO.java
@@ -24,6 +24,7 @@ public class UserDTO {
         this.password = password;
         this.phoneNumber = phoneNumber;
         this.tenantId = tenantId;
+        this.isActive = isActive;
         this.roles = roles == null ? new HashSet<>() : new HashSet<>(roles);
     }
 
